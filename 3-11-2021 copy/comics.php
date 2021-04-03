f9ce9a9e5db7606e8229cc4903666312a4500076
josh671 <jmb671@hotmail.com>
2021-04-03 12:04:35 -0700

    adding most recent files

---
+++ 3-11-2021 copy/comics.php
@@ -0,0 +1,59 @@
+<?php include('./includes/config.php'); ?> 
+<?php include('./includes/header.php'); ?> 
+<?php include('upload.php'); ?> 
+
+<div id="wrapper">
+<h1>Here are a list of my Comics</h1> 
+<table> 
+   <tr>
+       <th>Comics</th> 
+   </tr>
+   
+   <tr> 
+        <td><p class="td_left">#1 50th Page Party</p> <p class="td_right"> Date Added</p></td> 
+   </tr> 
+</table>
+
+</div>
+
+<div id="chapters_table">
+<?php 
+echo'<prev>';
+print_r($_POST['name']); 
+echo'</prev>';
+?>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+<!-- 
+-create test database and try to echo into table 
+-use appointments for reference 
+-------------------------------
+-first create disclaimer at top so you know the page 
+        wrapper
+        div for intro
+        some p tags with statements or something
+
+-set div for table 
+-connect to sql database using select * from databaseName
+-get $iconnect with the @mysqli_connect(DB_HOST, user, password , name ) or die(myerror(__FILE)); 
+
+
+
+
+
+ -->
+
+
+
+</body>
+</html>
